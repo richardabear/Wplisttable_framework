19bba6e6897bd9f718fddfa084e0ffbcf68adfcc
Richard Abear <owchzzz@gmail.com>
2015-06-19 14:51:47 +0800

    Final build ver 1.0.0

--- main.php
+++ main.php
@@ -1,10 +1,8 @@
-<?php
 
+<?php
 if ( ! class_exists( 'WP_List_Table' ) ) {
     require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
-
-
 class Techriver_maplists_list extends WP_List_Table{
 	protected $tablename; //Name of table you are going to use refer to contructor function
 	protected $per_page; //Items per page. Set in the constructor function
@@ -25,16 +23,19 @@ class Techriver_maplists_list extends WP_List_Table{
 		  
 		//Settings
 		$this->tablename = $wpdb->prefix . 'techriver_maplists'; //Change this to the table name of your data
-		$this->per_page = 10; //Change this to the number of items per page.
+		$this->per_page = 1; //Change this to the number of items per page.
 		
 		  
-		 $columns = array(
+		 $this->columns = array(
+			  'cb'      => '<input type="checkbox" />',
+			 'id' => 'ID',
+			 'name' => 'Name'
 		 );
 		
  
     }
 	
-	public static function get_data($per_page = 10, $page_number = 1) {
+	public function get_data($per_page = 10, $page_number = 1) {
 		global $wpdb;
 		
 		 $sql = "SELECT * FROM {$this->tablename}";
@@ -43,20 +44,14 @@ class Techriver_maplists_list extends WP_List_Table{
 		   $sql .= ' ORDER BY ' . esc_sql( $_REQUEST['orderby'] );
 		   $sql .= ! empty( $_REQUEST['order'] ) ? ' ' . esc_sql( $_REQUEST['order'] ) : ' ASC';
 		 }
-
 		 $sql .= " LIMIT $per_page";
-
 		 $sql .= ' OFFSET ' . ( $page_number - 1 ) * $per_page;
-
-
 		 $result = $wpdb->get_results( $sql, 'ARRAY_A' );
-
 		 return $result;
 	}
 	
 	public static function delete_data( $id ) {
 		  global $wpdb;
-
 		  $wpdb->delete(
 		    "{$this->tablename}",
 		    [ 'ID' => $id ],
@@ -94,7 +89,6 @@ class Techriver_maplists_list extends WP_List_Table{
 				return $item[ $column_name ]; //Show the default val
 		}
 	}
-
 	/**
 	 * Render the bulk edit checkbox
 	 *
@@ -104,11 +98,9 @@ class Techriver_maplists_list extends WP_List_Table{
 	 */
 	function column_cb( $item ) {
 		return sprintf(
-			'<input type="checkbox" name="bulk-delete[]" value="%s" />', $item['ID']
+			'<input type="checkbox" name="bulk-delete[]" value="%s" />', $item['id']
 		);
 	}
-
-
 	/**
 	 * Method for name column
 	 *
@@ -117,36 +109,22 @@ class Techriver_maplists_list extends WP_List_Table{
 	 * @return string
 	 */
 	function column_name( $item ) {
-
 		$delete_nonce = wp_create_nonce( 'sp_delete_customer' );
-
 		$title = '<strong>' . $item['name'] . '</strong>';
-
 		$actions = [
-			'delete' => sprintf( '<a href="?page=%s&action=%s&customer=%s&_wpnonce=%s">Delete</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['ID'] ), $delete_nonce )
+			'delete' => sprintf( '<a href="?page=%s&action=%s&customer=%s&_wpnonce=%s">Delete</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['id'] ), $delete_nonce )
 		];
-
 		return $title . $this->row_actions( $actions );
 	}
-
-
 	/**
 	 *  Associative array of columns
 	 *
 	 * @return array
 	 */
 	function get_columns() {
-		$columns = [
-			'cb'      => '<input type="checkbox" />',
-			'name'    => __( 'Name', 'sp' ),
-			'address' => __( 'Address', 'sp' ),
-			'city'    => __( 'City', 'sp' )
-		];
-
+		$columns = $this->columns;
 		return $columns;
 	}
-
-
 	/**
 	 * Columns to make sortable.
 	 *
@@ -155,12 +133,9 @@ class Techriver_maplists_list extends WP_List_Table{
 	public function get_sortable_columns() {
 		$sortable_columns = array(
 			'name' => array( 'name', true ),
-			'city' => array( 'city', false )
 		);
-
 		return $sortable_columns;
 	}
-
 	/**
 	 * Returns an associative array containing the bulk action
 	 *
@@ -170,72 +145,56 @@ class Techriver_maplists_list extends WP_List_Table{
 		$actions = [
 			'bulk-delete' => 'Delete'
 		];
-
 		return $actions;
 	}
-
-
 	/**
 	 * Handles data query and filter, sorting, and pagination.
 	 */
 	public function prepare_items() {
+		$columns = $this->columns;
+		$hidden = array(); //hidden columns
+		$sortable = $this->get_sortable_columns();
 		
-		$this->_column_headers = $this->get_column_info();
-
+		$this->_column_headers = array($columns,$hidden,$sortable);
 		/** Process bulk action */
 		$this->process_bulk_action();
-
 		$per_page     = $this->per_page;
 		$current_page = $this->get_pagenum();
 		$total_items  = self::record_count($this->tablename);
-
 		$this->set_pagination_args( [
 			'total_items' => $total_items, //WE have to calculate the total number of items
 			'per_page'    => $per_page //WE have to determine how many items to show on a page
 		] );
-
-		$this->items = self::get_customers( $per_page, $current_page );
+		$this->items = $this->get_data( $per_page, $current_page );
 	}
-
 	public function process_bulk_action() {
-
 		//Detect when a bulk action is being triggered...
 		if ( 'delete' === $this->current_action() ) {
-
 			// In our file that handles the request, verify the nonce.
 			$nonce = esc_attr( $_REQUEST['_wpnonce'] );
-
 			if ( ! wp_verify_nonce( $nonce, 'sp_delete_customer' ) ) {
 				die( 'Go get a life script kiddies' );
 			}
 			else {
 				self::delete_customer( absint( $_GET['customer'] ) );
-
 				wp_redirect( esc_url( add_query_arg() ) );
 				exit;
 			}
-
 		}
-
 		// If the delete bulk action is triggered
 		if ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-delete' )
 		     || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-delete' )
 		) {
-
 			$delete_ids = esc_sql( $_POST['bulk-delete'] );
-
 			// loop over the array of record IDs and delete them
 			foreach ( $delete_ids as $id ) {
 				self::delete_customer( $id );
-
 			}
-
 			wp_redirect( esc_url( add_query_arg() ) );
 			exit;
 		}
 	}
 }
-
 $map_lists_list = new Techriver_maplists_list();
 $map_lists_list->prepare_items();
 $map_lists_list->display();
